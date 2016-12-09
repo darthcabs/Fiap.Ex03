51a187556ff248bf03530e8f135d54188453639b
DAVID DE SOUZA CARDOSO <RM76860@fiap.com.br>
2016-12-09 18:34:18 -0200

    Authentication that comes by default

---
+++ Fiap.Ex05/Fiap.Exemplo05.Web.MVC/Fiap.Exemplo05.Web.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fiap.Exemplo05.Web.MVC.Startup))]
+namespace Fiap.Exemplo05.Web.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
